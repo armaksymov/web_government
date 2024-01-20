4ab35e97066e0422109fb81c53a8f5d0b0ab3101
melbasiouny <melbasiouny@pm.me>
2024-01-20 12:17:31 -0500

    Restructure the backend routing

--- run.py
+++ run.py
@@ -18,10 +18,10 @@ Note:
 """
 
 from flask import Flask
-from app.routes.services import services_blueprint
+from app.routes.main import main_blueprint
 
 app = Flask(__name__, template_folder="app/templates", static_folder="app/static")
-app.register_blueprint(services_blueprint)
+app.register_blueprint(main_blueprint)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
